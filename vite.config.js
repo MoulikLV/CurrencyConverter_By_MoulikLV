0316c6fdf642c8fed08ac794cd9dd677e8c0f570
MoulikLV <saimoulik05bunny@gmail.com>
2024-09-24 15:04:04 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Allow access from any IP
+    port: 5173,      // Specify your desired port
+    strictPort: true // If true, Vite will exit if the port is already in use
+  }
+});
